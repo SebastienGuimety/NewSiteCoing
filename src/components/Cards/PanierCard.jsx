b3f3ca3e65510cd4fbc9d3a82ed9c310b94ae67f
sebastienguimety <seb.guimety@gmail.com>
2024-07-07 19:55:08 +0200

    feat: Add PlusIcon and MinusIcon to PanierCard for quantity increment and decrement

--- src/components/Cards/PanierCard.jsx
+++ src/components/Cards/PanierCard.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import { removeItem, incrementQuantity, decrementQuantity } from '../../redux/cartSlice';
+import PlusIcon from '../../assets/svgs/PlusIcon';
+import MinusIcon from '../../assets/svgs/MinusIcon';
+import { PiTrashLight } from "react-icons/pi";
 
 const PanierCard = ({ item }) => {
   const dispatch = useDispatch();
@@ -12,18 +15,25 @@ const PanierCard = ({ item }) => {
       <div className="ml-4 flex-1">
         <h3 className="text-lg font-semibold">{item.title}</h3>
         <p>{        console.log(item)}</p>
-        <p>{(item.price * item.quantity).toFixed(2)} € <span className="text-gray-400">x {item.quantity}</span></p>
+        <p>{ item.price } € <span className="text-gray-400">x {item.quantity}</span></p>
        
       </div>
       <div className="flex items-center space-x-2">
-        <button className="bg-gray-200 text-black px-2 py-1 rounded" onClick={() => dispatch(incrementQuantity(item.id))}>
-          +
+        <button
+          onClick={() => dispatch(incrementQuantity(item.id))}
+          className="text-white px-2 py-2 flex items-center justify-center transition-colors duration-700 hover:bg-gray-800"
+        >
+          <PlusIcon size={20} />
         </button>
-        <button className="bg-gray-200 text-black px-2 py-1 rounded" onClick={() => dispatch(decrementQuantity(item.id))}>
-          -
+        
+        <button
+          onClick={() => dispatch(decrementQuantity(item.id))}
+          className="text-white px-2 py-2 flex items-center justify-center transition-colors duration-700 hover:bg-gray-800"
+        >
+          <MinusIcon size={20} />
         </button>
         <button className="text-white" onClick={() => dispatch(removeItem(item.id))}>
-          <FaTrashAlt className="text-xl" />
+          <PiTrashLight className="text-xl" />
         </button>
       </div>
     </div>
